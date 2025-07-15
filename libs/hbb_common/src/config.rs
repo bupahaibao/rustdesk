1ed83c5b16688ccc467f5140e407be9ad9ce8f6f
bupahaibao <67363931+bupahaibao@users.noreply.github.com>
2025-07-15 21:14:20 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.nanjing.han-qu.com"];
+pub const RS_PUB_KEY: &str = "u8iNnS11gQdArvWy0w34dXqPaZmiVc5ssAzadI3Qb7Y=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
